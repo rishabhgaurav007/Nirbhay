b69ea1852e845924dc50bdb77ae73cc2db740c38
KRISHNA KANT SINGH <kks1102000@gmail.com>
2020-02-15 11:31:36 +0530

    added login, signup for Users

--- app/src/main/java/com/example/nirbhay/MainActivity.java
+++ app/src/main/java/com/example/nirbhay/MainActivity.java
@@ -1,14 +1,129 @@
 package com.example.nirbhay;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.nirbhay.Users.CompleteSignUp;
+import com.example.nirbhay.Users.PostActivity;
+import com.example.nirbhay.Users.SignUpActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import static com.google.firebase.auth.FirebaseAuth.getInstance;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseAuth auth;
+    private EditText emailField, passwordField;
+    private ProgressDialog progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        setContentView(R.layout.activity_main);
+        emailField = (EditText) findViewById(R.id.emailSignIn);
+        passwordField = (EditText) findViewById(R.id.passwordSignIn);
+
+        auth = getInstance();
+
+
+
+
+        SharedPreferences preferences = getApplicationContext().getSharedPreferences("Pref", 0);
+        if(preferences!=null){
+            if(preferences.getBoolean("signedIn", false)){
+
+                if(!preferences.getBoolean("finishedSignUp", false)){
+                    Intent intent = new Intent(this, CompleteSignUp.class);
+                    startActivity(intent);
+                }else {
+
+                    Intent intent = new Intent(this, PostActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        }
+
+
+
+    }
+    private boolean validate(){
+        String password = passwordField.getText().toString();
+        String email = emailField.getText().toString();
+
+        if(email.isEmpty()){
+            // Toast.makeText(MainActivity.this, "Email "  +email, Toast.LENGTH_SHORT).show();
+
+            emailField.requestFocus();
+            emailField.setError("Required!!");
+            return false;
+        }
+        if(password.isEmpty()){
+            //Toast.makeText(MainActivity.this, "Pass "  +password, Toast.LENGTH_SHORT).show();
+
+            passwordField.requestFocus();
+            passwordField.setError("Required!!");
+            return false;
+        }
+        return true;
+    }
+    public void signInClick(View view) {
+
+        String password = passwordField.getText().toString();
+        String email = emailField.getText().toString();
+        if (validate()) {
+            signIn(email, password);
+        }
+    }
+
+    public void signIn(String email, String password){
+        progressDialog = new ProgressDialog(MainActivity.this);
+        progressDialog.setMessage("Signing in...");
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    progressDialog.hide();
+                    Toast.makeText(MainActivity.this, "Successfully Logged In", Toast.LENGTH_LONG).show();
+                    SharedPreferences pref = getApplicationContext().getSharedPreferences("Pref", 0);
+                    SharedPreferences.Editor editor = pref.edit();
+                    editor.putBoolean("finishedSignUp", true);
+                    editor.putBoolean("signedIn", true);
+                    editor.apply();
+                    editor.commit();
+
+                    Intent intent = new Intent(MainActivity.this, PostActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    progressDialog.hide();
+                    Toast.makeText(MainActivity.this, "Login failed: " + task.getException(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+    }
+    public void openSignupPage(View view) {
+        startActivity(new Intent(MainActivity.this, SignUpActivity.class));
     }
 }
---
+++ app/src/main/java/com/example/nirbhay/Users/SignUpActivity.java
@@ -0,0 +1,96 @@
+package com.example.nirbhay.Users;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.nirbhay.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private EditText emailField, passwordField;
+    private Button signUp;
+    private TextView back;
+    private FirebaseAuth firebaseAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        setContentView(R.layout.activity_sign_up);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        emailField = (EditText) findViewById(R.id.email);
+        passwordField = (EditText) findViewById(R.id.password);
+        signUp = (Button) findViewById(R.id.signUp);
+        back = (TextView) findViewById(R.id.back);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //final String name = nameField.getText().toString();
+                String email = emailField.getText().toString();
+                String password = passwordField.getText().toString();
+
+                if(!email.isEmpty() && !password.isEmpty() ){
+                    firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(
+                            SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if(!task.isSuccessful()){
+                                        Toast.makeText(SignUpActivity.this, "Sign Up Failed: "+task.getException().getMessage(),
+                                                Toast.LENGTH_LONG).show();
+                                    }
+                                    else{
+                                        SharedPreferences pref = getApplicationContext().getSharedPreferences("Pref", 0);
+                                        SharedPreferences.Editor editor = pref.edit();
+                                        editor.putBoolean("finishedSignUp", false);
+                                        editor.putBoolean("signedIn", true);
+                                        editor.apply();
+                                        editor.commit();
+                                        Intent intent = new Intent(SignUpActivity.this, CompleteSignUp.class);
+
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                }
+                            }
+                    );
+                }
+                else{
+                    if(email.isEmpty()){
+                        emailField.setError("Email Id Required");
+                        emailField.requestFocus();
+                    }
+                    if(password.isEmpty()){
+                        passwordField.setError("Password Required");
+                        passwordField.requestFocus();
+                    }
+                }
+            }
+        });
+
+    }
+}
